594d214eef64b6ba5a389e061bb98925cbae217e
leandro <leandrotancredi87@gmail.com>
2022-08-21 08:38:33 -0300

    primer commit

---
+++ tancredi-app/src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Nabvar from './componentes/Nabvar';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Nabvar/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+      </header>
+    </div>
+  );
+}
+<img src="" alt="" />
+
+export default App;
---
+++ tancredi-app/src/componentes/Nabvar.jsx
@@ -0,0 +1,25 @@
+
+const Nabvar=()=>{
+  return (
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnbVleXmeKmgo94t1KyrTC8ok065SiJ5C7E-yPooUNFXbfbqEehpdQ8EtJzxNAOqgF7m0&usqp=CAU" alt="" width="30" height="30" />
+        <a class="navbar-brand " href="#">Refrigeración La Plata</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Productos</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+export default Nabvar;
